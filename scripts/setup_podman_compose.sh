a958848357db950884cf769ea3e06cbfa927e6e3
Andrew Magid <magid.andrew@gmail.com>
2024-10-29 12:46:16 -0400

    omz support, bumped nvm version, exit when commands fail, switch to fuse-overlayfs if vfs for podman

--- scripts/setup_podman_compose.sh
+++ scripts/setup_podman_compose.sh
@@ -3,29 +3,50 @@
 # Function to display usage instructions
 usage() {
     echo "Usage: $0 -e EMAIL -p PROJECT_NAME"
-    echo "  -e EMAIL          Your email address for SSH key generation"
-    echo "  -p PROJECT_NAME   The name of your project"
+    echo "  -e EMAIL            Your email address for SSH key generation"
+    echo "  -p PROJECT_NAME     The name of your project on GitHub"
+    echo "  -g GITHUB_USERNAME  Your GitHub username"
     exit 1
 }
 
+die() {
+  if [ $? -ne 0 ]; then
+    echo "An error occurred. Exiting..."
+    exit 1
+  fi
+}
+
 # Parse command line arguments
-while getopts ":e:p:" opt; do
+while getopts ":e:p:g:" opt; do
   case $opt in
     e) EMAIL=$OPTARG ;;
     p) PROJECT_NAME=$OPTARG ;;
+    g) GITHUB_USERNAME=$OPTARG ;;
     *) usage ;;
   esac
 done
 
 # Check if email and project name are provided
-if [ -z "$EMAIL" ] || [ -z "$PROJECT_NAME" ]; then
+if [ -z "$EMAIL" || -z "$PROJECT_NAME" || -z "$GITHUB_USERNAME" ]; then
     usage
+fi
+
+echo "Does the following information look correct?"
+echo "Email: $EMAIL"
+echo "Project Name: $PROJECT_NAME"
+echo "GitHub Username: $GITHUB_USERNAME"
+read -p "Continue? y/n: " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
 fi
 
 # Update and install necessary packages
 echo "Updating package list and installing necessary packages..."
 sudo apt update
-sudo apt install -y podman python3-pip python3-venv git dbus slirp4netns
+sudo apt install -y podman python3-pip python3-venv git dbus slirp4netns zsh
+
+# Install omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Create and activate the virtual environment
 echo "Creating and activating virtual environment for Podman Compose..."
@@ -38,7 +59,18 @@ pip install podman-compose
 
 # Verify Podman Compose installation
 echo "Verifying Podman Compose installation..."
-podman-compose --version
+podman-compose --version || die "Podman Compose installation failed"
+
+# https://stackoverflow.com/questions/71081234/podman-builds-and-runs-containers-extremely-slow-compared-to-docker
+podman info --debug | grep graphDriverName | grep -q vfs
+if [ $? -eq 0 ]; then
+  echo "podman is using vfs instead of fuse-overlayfs. Switching to fuse-overlayfs..."
+  sudo apt install fuse-overlayfs
+  /usr/bin/fuse-overlayfs --version || die "fuse-overlayfs installation failed"
+  sudo mkdir -p /etc/containers
+  sudo echo -e "[storage]\n driver = \"overlay\"\n\n [storage.options]\n mount_program = \"/usr/bin/fuse-overlayfs\"" > /etc/containers/storage.conf
+  podman system reset
+fi
 
 # Automatically activate the virtual environment on SSH
 echo "Configuring shell to automatically activate the virtual environment on SSH login..."
@@ -52,6 +84,10 @@ fi
 # For Zsh
 if [ -f ~/.zshrc ]; then
     echo "source ~/podman_env/bin/activate" >> ~/.zshrc
+    # https://stackoverflow.com/questions/73077938/how-to-correctly-setup-plugin-nvm-for-oh-my-zsh
+    # add zstyle ':omz:plugins:nvm' autoload true to ~/.zshrc
+    # to make nvm work with oh-my-zsh
+    sed -i '/source $ZSH\/oh-my-zsh.sh/a\  zstyle '\'':omz:plugins:nvm'\'' autoload true' ~/.zshrc
     source ~/.zshrc
 fi
 
@@ -74,18 +110,21 @@ sudo chown $USER:$USER /etc/$PROJECT_NAME
 # Pull code from GitHub
 echo "Pulling code from GitHub..."
 cd /etc/$PROJECT_NAME
-git clone git@github.com:your_username/your_repository.git
+git clone "git@github.com:$GITHUB_USERNAME/$PROJECT_NAME.git"
 
 # Install NVM and Node.js
 echo "Installing NVM and Node.js..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+# Add nvm to the shell
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install --lts
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install --lts || die "nvm installation failed"
 
 # Verify Node.js installation
-node -v
-npm -v
+node -v || die "Node.js installation failed"
+npm -v || die "npm installation failed"
 
 # Configure default registries for Podman
 echo "Configuring default registries for Podman..."
@@ -99,7 +138,7 @@ echo "Installing Hasura CLI..."
 curl -L https://github.com/hasura/graphql-engine/raw/stable/cli/get.sh | bash
 
 # Verify Hasura CLI installation
-hasura version
+hasura version || die "Hasura CLI installation failed"
 
 # Modify net.ipv4.ip_unprivileged_port_start to allow rootless users to bind to ports 80 and 443
 echo "Modifying /etc/sysctl.conf to allow rootless users to bind to ports 80 and 443..."
@@ -126,3 +165,4 @@ EOF
 # Print completion message
 echo "Setup complete! You are now ready to use Podman Compose on your Debian instance."
 echo "Don't forget to add your SSH key to your GitHub account if you haven't already."
+
